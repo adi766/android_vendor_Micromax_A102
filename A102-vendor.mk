1a0e62a172dcdd1e285f5f198e3835637ddbe474
Aditya <adityakumarteli7846@gmail.com>
2017-05-10 12:44:04 +0530

    Initial commit

---
+++ A102-vendor.mk
@@ -0,0 +1,10 @@
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/Micromax/A102/proprietary/bin,system/bin)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/Micromax/A102/proprietary/lib,system/lib)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/Micromax/A102/proprietary/etc,system/etc)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/Micromax/A102/proprietary/vendor,system/vendor)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/Micromax/A102/proprietary/xbin,system/xbin)
+
---
+++ README.md
@@ -0,0 +1,2 @@
+# android_vendor_Micromax_A102
+This vendor tree is for Micromax A102,MT6572.
